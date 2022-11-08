4a6daf8d4cee7f9791696fd1c24eee3f5ad2c685
polyaria <polya.shmeliova@gmail.com>
2022-11-08 21:04:36 +0300

    bugs fixed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#define FLT_EPSILON 1.19209290e-07F // float
-#define LDBL_EPSILON 1.084202172485504434007452e-19L //long double
-#include <float.h>
+// КМБО-03-19 Шмелёва Полина
+// почта: polya.shmeliova@gmail.com
+// статья:https://www.researchgate.net/publication/337829551_A_simple_formula_for_solving_quadratic_equations_using_function_evaluation
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <limits>
 #include "excerpt.h"
 
 using namespace std;
@@ -24,40 +26,40 @@ void simple_formula(std::vector<fp_t> coefficients, std::vector<fp_t> &roots) {
         z = -b / (2 * a);
         //Формула: f_z = a * pow(z, 2) + b * z + c;
         fp_t fma_bzc = fma(b, z, c);
-        f_z = fma(a, pow(z, 2), fma_bzc);
-
-        //cout << "f_z = " << f_z<< endl;
-        //вычислим другими способами, чтоб посмотреть на точность
-        /*fp_t f_z1 = a * pow(z, 2) + b * z + c;
-        cout << "f_z1 = " << f_z1<< endl;
-        fp_t f_z2 = a * pow(z, 2) + fma_bzc;
-        cout << "f_z2 = " << f_z2<< endl;
-         */
+        f_z = fma(a, z*z, fma_bzc);
 
         if (f_z <= 0 ) {
-            roots[0] = z + sqrt(-f_z / a);
-            roots[1] = z - sqrt(-f_z / a);
+            fp_t sqrt_f = sqrt(-f_z / a);
+            //z >= 0 ? roots[0] = z + sqrt_f , roots[1] =  roots[0] - 2*sqrt_f : roots[0] = z - sqrt_f, roots[1] = roots[0] + 2*sqrt_f;
+
+            if(z >= 0) {
+                roots[0] = z + sqrt_f;
+                roots[1] =  roots[0] - 2*sqrt_f;
+            }
+            else{
+                roots[0] = z - sqrt_f;
+                roots[1] = roots[0] + 2*sqrt_f;
+            }
             cout << "\t\tReal roots: " << endl;
             cout << "\t x1 = " << roots[0] << "; x2 = " << roots[1] << endl;
         }
         else {
+
             std::complex<fp_t> sqrt_f_z = std::sqrt(std::complex<fp_t>((-f_z / a)));
             std::complex<fp_t> root1(z, sqrt_f_z.imag());
             std::complex<fp_t> root2(z, -sqrt_f_z.imag());
 
-            // выясним, действительно ли корень комплексный - попробуем отбросить комплексную часть и подставим в уравнение
-            // если результат будет > FLT_EPSILON - > корень комплексный, иначе - действительный и полученная
+            // выясним, действительно ли корень комплексный:
+            // если fabs(root1)*std::numeric_limits<fp_t>::epsilon() < fabs(root1.imag()) - > корень комплексный, иначе - действительный и полученная
             // комплексная часть - мусор...
 
-            fp_t fma_b_root_c = fma(b, root1.real(), c);
-            fp_t fma_res = fma(a, pow(root1.real(), 2),fma_b_root_c);
-            //cout << "fma_res = " << fma_res << endl;
-            if (fabs(fma_res) <= FLT_EPSILON)
+            if (fabs(root1)*std::numeric_limits<fp_t>::epsilon() > fabs(root1.imag()))
             {
-                roots[0] = z + sqrt(f_z / a);
-                roots[1] = z - sqrt(f_z / a);
+                //значит корень действительный
                 cout << "\t\tNot complex roots!"<<endl;
                 cout<< "\t\tReal roots: "<< endl;
+                roots[0] = roots[1] =  root1.real();
+
                 cout << "\t x1 = " << roots[0] << "; x2 = " << roots[1] << endl;
             }
             else {
@@ -74,7 +76,7 @@ auto testPolynomial(unsigned int roots_count) {
     fp_t deviation;
     vector<fp_t> roots_computed(roots_count);
     vector<fp_t> roots(roots_count), coefficients(roots_count + 1);
-    generate_polynomial<fp_t>(roots_count, 0, roots_count, 0, 1e-5, -1, 1, roots, coefficients);
+    generate_polynomial<fp_t>(roots_count, 0, roots_count, 0, std::numeric_limits<fp_t>::epsilon(), -1, 1, roots, coefficients);
     simple_formula(coefficients, roots_computed);
     auto result = compare_roots<fp_t>(roots_computed.size(), roots.size(), roots_computed, roots, deviation);
     switch (result) {
@@ -96,7 +98,7 @@ auto testPolynomial(unsigned int roots_count) {
 int main() {
 
     float deviation, max_deviation = 0;
-    for (auto i = 0; i < 10000; ++i) {
+    for (auto i = 0; i < 10'000; ++i) {
         deviation = testPolynomial<float>(2);
         cout << "deviation = " << deviation<< endl;
         if (deviation > max_deviation) {
@@ -106,17 +108,11 @@ int main() {
     cout<< endl<<"MAX_deviation = "<< max_deviation<<endl;
 
 
-    /*cout << endl<< "\t\tEXAMPLES: "<< endl;
-    //Close roots
-    //Coefficients - > c, b, a
-    vector<float> koef1 = {0.000000011, -0.00021, 1};
-    vector<float> r1(2);
-    simple_formula(koef1,r1);
-
     //Complex roots
     //1*x^2 + 0.163012*x + 0.00664325 = 0
-    vector<float> koef2 = {0.00664325, 0.163012, 1};
+    /*vector<float> koef2 = {0.00664325, 0.163012, 1};
     vector<float> r2(2);
     simple_formula(koef2,r2);
-     */
+    */
+
 }
